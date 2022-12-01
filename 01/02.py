bebc32a658f0cb0fffda563b111476d96c01ba23
Jake Voytko <jake@jaunty.tech>
2022-12-01 14:47:49 +0000

    Day 1 solutions in Python

---
+++ 01/02.py
@@ -0,0 +1,20 @@
+import heapq
+
+amounts = []
+
+sum = 0
+
+with open('01/input.txt', 'r') as data:
+  for line in data:
+    stripped_line = line.strip()
+    if len(stripped_line) == 0:
+      amounts.append(-sum)
+      sum = 0
+    else:
+      sum += int(stripped_line)
+
+amounts.append(-sum)
+
+heapq.heapify(amounts)
+
+print(-amounts[0] + -amounts[1] + -amounts[2])
