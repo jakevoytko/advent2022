63c53db7040841ff52bdee446c2291a6b6fe2ccf
Jake Voytko <jake@jaunty.tech>
2022-12-16 16:26:02 +0000

    Part 16
    I cached the best result per set of labels. For part 2, I calculated
    all of the possible states and then calculated the max flow for
    the disjoint ones.
    
    Little bit of __hash__ abuse. Every day (of Advent of Code) we stray
    further from god's light.

---
+++ 16/01.py
@@ -0,0 +1,87 @@
+import re
+import math
+
+class Valve:
+  def __init__(self, label, neighbors, rate):
+    self.label = label
+    self.neighbors = neighbors
+    self.rate = rate
+    self.opened = False
+
+allValves = {}
+
+with open('./16/input.txt', 'r') as data:
+  for line in data:
+    match = re.search(r'Valve (\w\w) has flow rate=(\d+); tunnels? leads? to valves? ((\w\w(, )?)+)', line.strip())
+    label = match.group(1)
+    rate = int(match.group(2))
+    neighbors = match.group(3).split(', ')
+    allValves[label] = Valve(label, neighbors, rate)
+
+
+# Floyd-Warshal algorithm to calculate the distance between each pair of valves.
+travelMap = {a:{b:math.inf for b in allValves.keys()} for a in allValves.keys()}
+for destinationLabel, valve in allValves.items():
+  travelMap[destinationLabel][destinationLabel] = 0
+  for neighbor in valve.neighbors:
+    travelMap[destinationLabel][neighbor] = 1
+for k in allValves.keys():
+  for i in allValves.keys():
+    for j in allValves.keys():
+      if travelMap[i][j] > travelMap[i][k] + travelMap[k][j]:
+        travelMap[i][j] = travelMap[i][k] + travelMap[k][j]
+
+# Run multiple simulations in parallel in rounds. In each round, look at each of the simulations
+# and calculate how long it takes to traverse to all of the unvisited.
+# Deduplicate the simulations by sorting the list of already-visited nodes in each simulation
+# and computing hash values. Keep the conflicting simulation with the largest flow value.
+
+class SimulationState:
+  def __init__(self, currentValve, alreadyVisited, unvisited, timeRemaining, totalFlow):
+    self.currentValve = currentValve
+
+    self.alreadyVisited = alreadyVisited
+    self.alreadyVisited.sort()
+
+    self.unvisited = unvisited
+    self.timeRemaining = timeRemaining
+    self.totalFlow = totalFlow
+
+  def __hash__(self): # Abuse hash out of laziness
+    return hash(tuple(self.alreadyVisited))
+
+originState = SimulationState('AA', [], set([valve.label for valve in allValves.values() if valve.rate > 0]), 30, 0)
+simulationStates = {originState: originState}
+
+maxFlow = 0
+while True:
+  nextSimulationStates = {}
+
+  for state in simulationStates:
+    currentValve = allValves[state.currentValve]
+    for destinationLabel in state.unvisited:
+      destinationValve = allValves[destinationLabel]
+      timeAfterTraversalAndOpen = state.timeRemaining - travelMap[state.currentValve][destinationLabel] - 1
+      if timeAfterTraversalAndOpen < 0:
+        continue
+
+      alreadyVisited = state.alreadyVisited[:]
+      alreadyVisited.append(destinationLabel)
+      unvisited = set(state.unvisited)
+      unvisited.remove(destinationLabel)
+
+      flow = state.totalFlow + timeAfterTraversalAndOpen * destinationValve.rate
+      maxFlow = max(maxFlow, flow)
+
+      newState = SimulationState(destinationLabel, alreadyVisited, unvisited, timeAfterTraversalAndOpen, flow)
+      if newState in nextSimulationStates:
+        if newState.totalFlow > nextSimulationStates[newState]:
+          nextSimulationStates[newState] = newState
+      else:
+        nextSimulationStates[newState] = newState
+
+  if len(nextSimulationStates) == 0:
+    break
+  simulationStates = nextSimulationStates
+
+print(maxFlow)
